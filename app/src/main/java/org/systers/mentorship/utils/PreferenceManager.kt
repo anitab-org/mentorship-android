43c32ae402f14154a5038acf96c38139c77e6ca5
m-murad <murad.kuka@gmail.com>
2018-07-26 18:03:51 +0530

    feat: add preferencemanager class

---
+++ app/src/main/java/org/systers/mentorship/utils/PreferenceManager.kt
@@ -0,0 +1,24 @@
+package org.systers.mentorship.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import org.systers.mentorship.MentorshipApplication
+
+/**
+ * Created by murad on 7/26/18.
+ */
+class PreferenceManager {
+
+    companion object {
+        const val APPLICATION_PREFERENCE = "auth_token"
+        const val AUTH_TOKEN = "auth_token"
+    }
+
+    private val context: Context = MentorshipApplication.getContext()
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+            APPLICATION_PREFERENCE, Context.MODE_PRIVATE)
+
+    var authToken: String
+        set(authToken) = sharedPreferences.edit().putString(AUTH_TOKEN, "Bearer $authToken").apply()
+        get() = sharedPreferences.getString(AUTH_TOKEN, "")
+}
