b3a90d67ad9307f1426a206d0863c3a386df3049
m-murad <murad.kuka@gmail.com>
2018-07-26 18:35:59 +0530

    fix: fix type and add SerializedName annotation

--- app/src/main/java/org/systers/mentorship/remote/responses/LoginResponse.kt
+++ app/src/main/java/org/systers/mentorship/remote/responses/LoginResponse.kt
@@ -1,7 +1,9 @@
 package org.systers.mentorship.remote.responses
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by murad on 7/26/18.
  */
-data class LoginResponse(var authToken: String? = null,
-                         var expiry: Long? = null)
+data class LoginResponse(@SerializedName("access_token") var authToken: String? = null,
+                         var expiry: Float? = null)
