8892a8b624759b5819b8646939304d1554526193
m-murad <murad.kuka@gmail.com>
2018-07-26 18:01:42 +0530

    feat: add rx, retrofit and okhttp dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -15,6 +15,11 @@ object Versions {
     val junit = "4.12"
     val testRunner = "1.0.2"
     val espresso = "3.0.2"
+    val rxJava = "2.1.8"
+    val rxAndroid = "2.0.2"
+    val rxKotlin = "2.2.0"
+    val retrofitVersion = "2.3.0"
+    val okHttp3Version = "3.8.0"
 }
 
 object Dependencies {
@@ -26,4 +31,11 @@ object Dependencies {
     val junit = "junit:junit:${Versions.junit}"
     val test_runner = "com.android.support.test:runner:${Versions.testRunner}"
     val espresso = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
+    val rx_java = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
+    val rx_android = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+    val rx_kotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlin}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
+    val retrofit_gson_converter = "com.squareup.retrofit2:converter-gson:${Versions.retrofitVersion}"
+    val retrofit_rxjava2_adapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofitVersion}"
+    val okhttp3_logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp3Version}"
 }
