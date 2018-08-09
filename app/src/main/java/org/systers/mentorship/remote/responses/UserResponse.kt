52c20185a68478a7af5be124cca043b2f4807a8b
Isabel Costa <isabelcmdcosta@gmail.com>
2018-08-09 18:17:39 +0100

    feat: members list, other user profile UI

---
+++ app/src/main/java/org/systers/mentorship/remote/responses/UserResponse.kt
@@ -0,0 +1,34 @@
+package org.systers.mentorship.remote.responses
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * This data class represents all the information related to a user of the system
+ *
+ * @param id identifier of the user
+ * @param username username of the user
+ * @param name name of the user
+ * @param bio bio of the user
+ * @param location location, e.g., country or/and city where the user lives
+ * @param occupation current occupation of the user
+ * @param organization organization to which the user might belong
+ * @param interests interests the user possesses
+ * @param skills skills the user possesses
+ * @param needsMentoring true, if user wants to be mentored, false if otherwise
+ * @param isAvailableToMentor true, if user is available to mentor, false if otherwise
+ * @param slackUsername Slack username
+ */
+data class UserResponse(
+        var id: Int,
+        var username: String,
+        var name: String,
+        var bio: String,
+        var location: String,
+        var occupation: String,
+        var organization: String,
+        var interests: String,
+        var skills: String,
+        @SerializedName("need_mentoring") var needsMentoring: Boolean,
+        @SerializedName("available_to_mentor") var isAvailableToMentor: Boolean,
+        @SerializedName("slack_username") var slackUsername: String)
+
---
+++ app/src/main/java/org/systers/mentorship/viewmodels/MemberProfileViewModel.kt
@@ -0,0 +1,72 @@
+package org.systers.mentorship.viewmodels
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.annotations.NonNull
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+import org.systers.mentorship.MentorshipApplication
+import org.systers.mentorship.R
+import org.systers.mentorship.remote.datamanager.UserDataManager
+import org.systers.mentorship.remote.responses.UserResponse
+import org.systers.mentorship.utils.CommonUtils
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.concurrent.TimeoutException
+
+/**
+ * This class represents the [ViewModel] component used for the MemberProfileActivity
+ */
+class MemberProfileViewModel : ViewModel() {
+
+    var TAG = MemberProfileViewModel::class.java.simpleName
+
+    private val userDataManager: UserDataManager = UserDataManager()
+
+    val successful: MutableLiveData<Boolean> = MutableLiveData()
+    lateinit var message: String
+    lateinit var userProfile: UserResponse
+
+    /**
+     * Fetches profile from a user with id [userId] by calling getUser method from UserService
+     * @param userId id of a member
+     */
+    fun getUserProfile(@NonNull userId: Int) {
+        userDataManager.getUserById(userId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableObserver<UserResponse>() {
+                    override fun onNext(userResponse: UserResponse) {
+                        userProfile = userResponse
+                        successful.value = true
+                    }
+
+                    override fun onError(throwable: Throwable) {
+                        when (throwable) {
+                            is IOException -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_please_check_internet)
+                            }
+                            is TimeoutException -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_request_timed_out)
+                            }
+                            is HttpException -> {
+                                message = CommonUtils.getErrorResponse(throwable).message.toString()
+                            }
+                            else -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_something_went_wrong)
+                                Log.e(TAG, throwable.localizedMessage)
+                            }
+                        }
+                        successful.value = false
+                    }
+
+                    override fun onComplete() {
+                    }
+                })
+    }
+}
