93a511826deb7bb8dd691334d5c6ce741ef29529
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2018-10-26 09:21:04 +0530

    feat: add mentor and mentee availability option to sign up
    Fixes issue #64 by adding two checkboxes to indicate availability as
    Mentor/Mentee while signing up. Modifes the activity layout, as well
    RegisterRequest data class, SignUpActivity to include these two state
    variables in the register-request to the server.

--- app/src/main/java/org/systers/mentorship/remote/requests/RegisterRequest.kt
+++ app/src/main/java/org/systers/mentorship/remote/requests/RegisterRequest.kt
@@ -16,5 +16,7 @@ data class RegisterRequest (
         val username: String,
         val email: String,
         val password: String,
-        @SerializedName("terms_and_conditions_checked") val hasAcceptedTermsAndConditions: Boolean
+        @SerializedName("terms_and_conditions_checked") val hasAcceptedTermsAndConditions: Boolean,
+        @SerializedName("need_mentoring") val needsMentoring: Boolean,
+        @SerializedName("available_to_mentor") val isAvailableToMentor: Boolean
 )
