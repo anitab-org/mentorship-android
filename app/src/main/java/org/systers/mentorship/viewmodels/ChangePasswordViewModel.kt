6699c52e347277a6282166d1b656260125660fdc
ak8527 <ashutoshkumar1320@gmail.com>
2019-02-06 05:21:51 +0530

    feat: Add change password in Setting Screen

---
+++ app/src/main/java/org/systers/mentorship/viewmodels/ChangePasswordViewModel.kt
@@ -0,0 +1,67 @@
+package org.systers.mentorship.viewmodels
+
+import android.annotation.SuppressLint
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+import org.systers.mentorship.MentorshipApplication
+import org.systers.mentorship.R
+import org.systers.mentorship.remote.datamanager.UserDataManager
+import org.systers.mentorship.remote.requests.ChangePassword
+import org.systers.mentorship.remote.responses.CustomResponse
+import org.systers.mentorship.utils.CommonUtils
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.concurrent.TimeoutException
+
+/**
+ * This class represents the [ViewModel] used for ChangePasswordFragment
+ */
+class ChangePasswordViewModel : ViewModel() {
+
+    private val userDataManager: UserDataManager = UserDataManager()
+    val successfulUpdate: MutableLiveData<Boolean> = MutableLiveData()
+    lateinit var message: String
+
+    /**
+     * Updates the password of the current user
+     */
+    @SuppressLint("CheckResult")
+    fun changeUserPassword(changePassword: ChangePassword) {
+        userDataManager.updatePassword(changePassword)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableObserver<CustomResponse>() {
+                    override fun onNext(customResponse: CustomResponse) {
+                        message = customResponse.message
+                        successfulUpdate.value = true
+                    }
+
+                    override fun onError(e: Throwable) {
+                        when (e) {
+                            is IOException -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_please_check_internet)
+                            }
+                            is TimeoutException -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_request_timed_out)
+                            }
+                            is HttpException -> {
+                                message = CommonUtils.getErrorResponse(e).message
+                            }
+                            else -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_something_went_wrong)
+                            }
+                        }
+                        successfulUpdate.value = false
+                    }
+
+                    override fun onComplete() {
+                    }
+                })
+    }
+}
