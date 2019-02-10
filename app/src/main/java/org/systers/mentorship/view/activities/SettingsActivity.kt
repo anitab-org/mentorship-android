d43c7624d8750bcd555b39eacdf06d14e5676ec4
sashrikakaur <sashrikakaur@yahoo.co.in>
2019-02-10 22:22:43 +0530

    added code for on-click feature

--- app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
@@ -2,9 +2,9 @@ package org.systers.mentorship.view.activities
 
 import android.app.PendingIntent.getActivity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.systers.mentorship.R
 import org.systers.mentorship.utils.PreferenceManager
@@ -20,19 +20,21 @@ class SettingsActivity : BaseActivity() {
         supportActionBar?.title = getString(R.string.settings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
         supportActionBar?.setDisplayShowHomeEnabled(true);
-        tvAbout.setOnClickListener{
-            startActivity(Intent(this,MemberProfileActivity::class.java))
-        }
-
-        tvFeedback.setOnClickListener{
-            Toast.makeText(this, "Feedback",
-                    Toast.LENGTH_LONG).show();
-        }
 
         tvLogout.setOnClickListener {
-            preferenceManager.clear()
-            startActivity(Intent(this, LoginActivity::class.java))
-            finishAffinity()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.confirm_logout)
+            builder.setMessage(R.string.confirm_logout_msg)
+            builder.setPositiveButton(R.string.logout) { _, _ ->
+                preferenceManager.clear()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finishAffinity()
+            }
+            builder.setNegativeButton(R.string.cancel) {dialog, _ ->
+                dialog.cancel()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
         }
     }
     override fun onSupportNavigateUp(): Boolean {
