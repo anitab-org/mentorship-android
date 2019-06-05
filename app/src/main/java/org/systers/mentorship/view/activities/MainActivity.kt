e330a35740277d264c867b94144f49f8a37ac450
rjain90 <rjain90@live.co.uk>
2019-06-05 20:43:22 +0530

    fix: toast on exit

--- app/src/main/java/org/systers/mentorship/view/activities/MainActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/MainActivity.kt
@@ -2,23 +2,29 @@ package org.systers.mentorship.view.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_main.*
 import org.systers.mentorship.R
 import org.systers.mentorship.utils.PreferenceManager
 import org.systers.mentorship.view.fragments.*
+import java.util.concurrent.TimeUnit
 
 /**
  * This activity has the bottom navigation which allows the user to switch between fragments
  */
-class MainActivity: BaseActivity() {
+class MainActivity : BaseActivity() {
 
     private var atHome = true
 
     private val preferenceManager: PreferenceManager = PreferenceManager()
+
+    private val source = PublishSubject.create<Boolean>()
+    private val EXIT_TIMEOUT = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,44 +37,51 @@ class MainActivity: BaseActivity() {
         } else {
             atHome = savedInstanceState.getBoolean("atHome")
         }
+
+        source.observeOn(AndroidSchedulers.mainThread())
+                .doOnNext { Toast.makeText(this, R.string.application_exit_message, Toast.LENGTH_SHORT).show() }
+                .timeInterval(TimeUnit.MILLISECONDS)
+                .skip(1)
+                .filter { it.time() < EXIT_TIMEOUT }
+                .subscribe { finish() }
     }
 
     private val mOnNavigationItemSelectedListener =
             BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                replaceFragment(R.id.contentFrame, HomeFragment.newInstance(),
-                        R.string.fragment_title_home)
-                atHome = true
-                return@OnNavigationItemSelectedListener true
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        replaceFragment(R.id.contentFrame, HomeFragment.newInstance(),
+                                R.string.fragment_title_home)
+                        atHome = true
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_profile -> {
+                        replaceFragment(R.id.contentFrame, ProfileFragment.newInstance(),
+                                R.string.fragment_title_profile)
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_relation -> {
+                        replaceFragment(R.id.contentFrame, RelationPagerFragment.newInstance(),
+                                R.string.fragment_title_relation)
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_members -> {
+                        replaceFragment(R.id.contentFrame, MembersFragment.newInstance(),
+                                R.string.fragment_title_members)
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_requests -> {
+                        replaceFragment(R.id.contentFrame, RequestsFragment.newInstance(),
+                                R.string.fragment_title_requests)
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                }
+                false
             }
-            R.id.navigation_profile -> {
-                replaceFragment(R.id.contentFrame, ProfileFragment.newInstance(),
-                        R.string.fragment_title_profile)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_relation -> {
-                replaceFragment(R.id.contentFrame, RelationPagerFragment.newInstance(),
-                        R.string.fragment_title_relation)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_members -> {
-                replaceFragment(R.id.contentFrame, MembersFragment.newInstance(),
-                        R.string.fragment_title_members)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_requests -> {
-                replaceFragment(R.id.contentFrame, RequestsFragment.newInstance(),
-                        R.string.fragment_title_requests)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
@@ -100,7 +113,7 @@ class MainActivity: BaseActivity() {
         if (!atHome) {
             showHomeFragment()
         } else {
-            super.onBackPressed()
+            source.onNext(true)
         }
     }
 }
