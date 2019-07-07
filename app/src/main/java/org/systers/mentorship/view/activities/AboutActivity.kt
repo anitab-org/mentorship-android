4abeb5965eb7c8b3a1ce85d3a607731399b5f253
Saumya Singh <ssaumyas100@gmail.com>
2019-07-08 01:47:42 +0530

    feat: add About activity (#219)

---
+++ app/src/main/java/org/systers/mentorship/view/activities/AboutActivity.kt
@@ -0,0 +1,38 @@
+package org.systers.mentorship.view.activities
+
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_about.*
+import org.systers.mentorship.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        btnGit.setOnClickListener {
+
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_github))))
+        }
+        btnSlack.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_slack))))
+        }
+        btnWebsite.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_website))))
+        }
+        btnTermsCondition.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_terms))))
+        }
+        btnprivacypolicy.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_privacy))))
+        }
+        btncodeofconduct.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_code_of_conduct))))
+        }
+
+    }
+}
