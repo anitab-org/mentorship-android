170f79065489e3463dbe59576abe896b4f7dffcd
Jennifer <jenpaff0@gmail.com>
2019-07-31 22:03:17 +0200

    feat: fetch all tasks for mentorship-relation (#231)
    * feat: fetch all tasks for mentorship-relation
    * create TaskService
    * create TaskDataManager
    * call API to fetch all tasks from DB
    * small UI changes -> add new task button

--- app/src/main/java/org/systers/mentorship/viewmodels/TaskViewModel.kt
+++ app/src/main/java/org/systers/mentorship/viewmodels/TaskViewModel.kt
@@ -1,24 +1,73 @@
 package org.systers.mentorship.viewmodels
 
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 import org.systers.mentorship.MentorshipApplication
 import org.systers.mentorship.R
+import org.systers.mentorship.models.Task
+import org.systers.mentorship.remote.datamanager.TaskDataManager
+import org.systers.mentorship.utils.CommonUtils
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.concurrent.TimeoutException
 
 /**
  * This class represents the [ViewModel] used for Tasks Screen
  */
 class TasksViewModel: ViewModel() {
-    var taskList = mutableListOf<String>()
-    var completedTaskList = mutableListOf<String>()
+
+    var TAG = TasksViewModel::class.java.simpleName
+
+    lateinit var tasksList: List<Task>
+
+    private val taskDataManager: TaskDataManager = TaskDataManager()
+    val successful: MutableLiveData<Boolean> = MutableLiveData()
+    lateinit var message: String
 
     /**
      * This function lists all tasks from the mentorship relation
      */
-    fun getTasks() {
-        taskList.add(MentorshipApplication.getContext().getString(R.string.model_task_1))
-        taskList.add(MentorshipApplication.getContext().getString(R.string.model_task_2))
-        taskList.add(MentorshipApplication.getContext().getString(R.string.model_task_3))
-        //TODO: Fetch the list from the backend
+    @SuppressLint("CheckResult")
+    fun getTasks(relationId: Int) {
+        taskDataManager.getAllTasks(relationId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableObserver<List<Task>>() {
+                    override fun onNext(taskListResponse: List<Task>) {
+                        tasksList = taskListResponse
+                        successful.value = true
+                    }
+
+                    override fun onError(throwable: Throwable) {
+                        when (throwable) {
+                            is IOException -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_please_check_internet)
+                            }
+                            is TimeoutException -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_request_timed_out)
+                            }
+                            is HttpException -> {
+                                message = CommonUtils.getErrorResponse(throwable).message.toString()
+                            }
+                            else -> {
+                                message = MentorshipApplication.getContext()
+                                        .getString(R.string.error_something_went_wrong)
+                                Log.e(TAG, throwable.localizedMessage)
+                            }
+                        }
+                        successful.value = false
+                    }
+
+                    override fun onComplete() {
+                    }
+                })
     }
 
     /**
@@ -26,7 +75,6 @@ class TasksViewModel: ViewModel() {
      * @param taskName title of the new task
      */
     fun addTask(taskName: String) {
-        taskList.add(taskName)
         //TODO: Update the backend
     }
 
@@ -37,11 +85,11 @@ class TasksViewModel: ViewModel() {
      */
     fun updateTask(taskId: Int, isChecked: Boolean){
         if(isChecked) {
-            completedTaskList.add(taskList.get(taskId))
+            //completedTaskList.add(taskList.get(taskId))
             //TODO: Update the backend
         }
         else {
-            completedTaskList.remove(taskList.get(taskId))
+            //completedTaskList.remove(taskList.get(taskId))
             //TODO: Update the backend
         }
     }
