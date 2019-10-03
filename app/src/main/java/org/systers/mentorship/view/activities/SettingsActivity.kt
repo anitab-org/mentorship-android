6ea9d40e4ce586cabb43434f1c5c8656ce447b20
nsprm <nsprm@amazon.com>
2019-10-03 11:46:02 -0700

    Delete account by clicking delete button

--- app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
@@ -2,15 +2,21 @@ package org.systers.mentorship.view.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.systers.mentorship.R
 import org.systers.mentorship.utils.PreferenceManager
 import org.systers.mentorship.view.fragments.ChangePasswordFragment
+import org.systers.mentorship.viewmodels.DeleteUserViewModel
 
 class SettingsActivity : BaseActivity() {
 
     private val preferenceManager: PreferenceManager = PreferenceManager()
+    private lateinit var deleteUserViewModel: DeleteUserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +26,27 @@ class SettingsActivity : BaseActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        deleteUserViewModel = ViewModelProviders.of(this).get(DeleteUserViewModel::class.java)
+        deleteUserViewModel.successfulDelete.observe(this, Observer { successful ->
+
+            if (successful != null) {
+                when {
+                    successful -> {
+                        Toast.makeText(this, getString(R.string.user_deleted), Toast.LENGTH_SHORT).show()
+                        logout()
+                    }
+                    else -> Toast.makeText(this, deleteUserViewModel.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+
         tvLogout.setOnClickListener {
             val builder = AlertDialog.Builder(this)
             builder.setTitle(R.string.confirm_logout)
             builder.setMessage(R.string.confirm_logout_msg)
             builder.setPositiveButton(R.string.logout) { _, _ ->
-                preferenceManager.clear()
-                startActivity(Intent(this, LoginActivity::class.java))
-                finishAffinity()
+                logout()
             }
             builder.setNegativeButton(R.string.cancel) { dialog, _ ->
                 dialog.cancel()
@@ -39,9 +58,31 @@ class SettingsActivity : BaseActivity() {
         tvResetPassword.setOnClickListener {
             ChangePasswordFragment.newInstance().show(supportFragmentManager, null)
         }
+
         tvAbout.setOnClickListener {
-            startActivity(Intent(baseContext,AboutActivity::class.java))
+            startActivity(Intent(baseContext, AboutActivity::class.java))
         }
+
+        tvDeleteAccount.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.confirm_delete)
+            builder.setMessage(R.string.confirm_delete_msg)
+            builder.setPositiveButton(R.string.delete) { _, _ ->
+                deleteUserViewModel.deleteUser()
+            }
+            builder.setNegativeButton(R.string.cancel) { dialog, _ ->
+                dialog.cancel()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+
+    }
+
+    fun logout(){
+        preferenceManager.clear()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finishAffinity()
     }
 
     override fun onSupportNavigateUp(): Boolean {
