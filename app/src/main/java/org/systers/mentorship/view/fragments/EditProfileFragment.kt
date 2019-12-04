3b7a5f48456f70725b1c680bf5b08e2d9d91b2ba
daksha-aeer <dakshaaeer@gmail.com>
2019-12-04 19:40:16 +0530

    feat: Make "Upload Profile Picture" option clickable
    Upload from gallery and take picture functionality added
    Files updated: fragment_edit_profile.xml, EditProfileFragment.kt
    
    Working
    1. xml names: imgUserAvatar(ImageView), profilePictureButton(FloatingActionButton)
    2. listener selectImage() attached to button in onResume() function
    3. selectImage(): Checks for CAMERA and WRITE_EXTERNAL_STORAGE permissions. It then displays
       AlertDialog with 3 options- take photo, go to gallery and cancel. These activate respective
       intents.
    4. onActivityResult() is triggered when intents are executed. It updates image in imgUserAvatar.

--- app/src/main/java/org/systers/mentorship/view/fragments/EditProfileFragment.kt
+++ app/src/main/java/org/systers/mentorship/view/fragments/EditProfileFragment.kt
@@ -1,18 +1,31 @@
 package org.systers.mentorship.view.fragments
 
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.DialogFragment
 import androidx.appcompat.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
 import org.systers.mentorship.R
 import org.systers.mentorship.databinding.FragmentEditProfileBinding
 import org.systers.mentorship.models.User
@@ -78,8 +91,16 @@ class EditProfileFragment: DialogFragment() {
 
     override fun onResume() {
         super.onResume()
-
         val editProfileDialog = dialog as AlertDialog
+        editProfileDialog.profilePictureButton.setOnClickListener{selectImage()}
+        /* "upload profile picture" button working
+        1. xml names: imgUserAvatar(ImageView), profilePictureButton(FloatingActionButton)
+        2. listener selectImage() attached to button in onResume() function
+        3. selectImage(): Checks for CAMERA and WRITE_EXTERNAL_STORAGE permissions. It then displays
+           AlertDialog with 3 options- take photo, go to gallery and cancel. These activate respective
+           intents.
+        4. onActivityResult() is triggered when intents are executed. It updates image in imgUserAvatar.
+         */
 
         editProfileDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
             val errors = validateProfileInput(editProfileBinding.user?.name?.trim())
@@ -107,14 +128,12 @@ class EditProfileFragment: DialogFragment() {
             }
         }
     }
-
     override fun onDestroy() {
         super.onDestroy()
 
         profileViewModel.successfulUpdate.removeObservers(activity!!)
         profileViewModel.successfulUpdate.value = null
     }
-
     private fun validateProfileInput(name: String?): Array<EditProfileFragmentErrorStates> {
 
         var errors = arrayOf<EditProfileFragmentErrorStates>()
@@ -129,5 +148,69 @@ class EditProfileFragment: DialogFragment() {
             errors += EditProfileFragmentErrorStates.NameTooLongError
         }
         return errors
+    }
+    private fun selectImage(){
+        if (checkPermissions()){
+            val options = arrayOf<CharSequence>(getString(R.string.select_image_camera),
+                    getString(R.string.select_image_gallery), getString(R.string.select_image_cancel))
+            val builder= AlertDialog.Builder(context!!)
+            builder.setItems(options){
+                dialog,item ->
+                when(options[item]){
+                    getString(R.string.select_image_camera)-> {
+                        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(takePicture, 0)
+                    }
+                    getString(R.string.select_image_gallery)-> {
+                        val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        startActivityForResult(pickPhoto, 1)
+                    }
+                    getString(R.string.select_image_cancel)-> {
+                        dialog.dismiss()
+                    }
+                }
+            }
+            builder.show()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_CANCELED) {
+            val editProfileDialog = dialog as AlertDialog
+            val imageView = editProfileDialog.imgUserAvatar as ImageView
+            when (requestCode) {
+                0 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage = data.extras!!.get("data") as Bitmap
+                    imageView.setImageBitmap(selectedImage)
+                }
+                1 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage = data.data
+                    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                    if (selectedImage != null) {
+                        val cursor = context!!.getContentResolver().query(selectedImage,
+                                filePathColumn, null, null, null)
+
+                        if (cursor != null) {
+                            cursor.moveToFirst()
+                            val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+                            val imgDecodableString = cursor.getString(columnIndex)
+                            cursor.close()
+                            imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun checkPermissions(): Boolean {
+        if ((ContextCompat.checkSelfPermission(context!!, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) ||
+                (ContextCompat.checkSelfPermission(context!!, CAMERA) != PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(activity!!, arrayOf(WRITE_EXTERNAL_STORAGE, CAMERA),
+                    1001)
+            println(getString(R.string.permission_granted))
+            return false
+        } else {
+            println(getString(R.string.permission_rejected))
+        }
+        return true
     }
 }
