d68889ea625f191ab9f5f8c3d62893051705b468
Rudraksh Jhalani <rudrakshjhalani24@gmail.com>
2019-12-05 01:16:52 +0530

    feat: Added the 'Find Members' button

--- app/src/main/java/org/systers/mentorship/view/fragments/RelationPagerFragment.kt
+++ app/src/main/java/org/systers/mentorship/view/fragments/RelationPagerFragment.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_relation.*
 import org.systers.mentorship.R
 import org.systers.mentorship.models.Relationship
@@ -48,6 +49,14 @@ class RelationPagerFragment : BaseFragment() {
                 }
             }
         })
+
+        btnFindMembers.setOnClickListener {
+            activity?.bottomNavigation?.menu?.findItem(R.id.navigation_members)?.setChecked(true)
+            fragmentManager
+                    ?.beginTransaction()
+                    ?.replace(R.id.contentFrame,MembersFragment.newInstance(),"Members")
+                    ?.commit()
+        }
 
         activityCast.showProgressDialog(getString(R.string.fetching_current_relation))
         relationViewModel.getCurrentRelationDetails()
