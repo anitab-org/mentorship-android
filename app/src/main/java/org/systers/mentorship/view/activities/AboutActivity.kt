c5052329a42a3ab4cda180286b6dc6ce9ab7e10f
Se7eN <asharalikhan200@gmail.com>
2019-12-05 22:23:12 +0530

    feat: integrate WebView in AboutActivity

--- app/src/main/java/org/systers/mentorship/view/activities/AboutActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/AboutActivity.kt
@@ -1,43 +1,63 @@
 package org.systers.mentorship.view.activities
 
-import android.content.Intent
-import android.content.res.Resources
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_about.*
 import org.systers.mentorship.R
 
-class AboutActivity : AppCompatActivity() {
+
+class AboutActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var clearHistory = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-        setTitle(R.string.fragment_title_about);
-
+        setTitle(R.string.fragment_title_about)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        webView.webViewClient = object: WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                if (clearHistory) {
+                    clearHistory = false
+                    view.clearHistory()
+                }
+                super.onPageFinished(view, url)
 
-        btnGit.setOnClickListener {
-
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_github))))
-        }
-        btnSlack.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_slack))))
-        }
-        btnWebsite.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_website))))
-        }
-        btnTermsCondition.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_terms))))
-        }
-        btnprivacypolicy.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_privacy))))
-        }
-        btncodeofconduct.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_code_of_conduct))))
+                showWebView()
+            }
         }
 
+        hideWebView()
+
+        btnGit.setOnClickListener(this)
+        btnSlack.setOnClickListener(this)
+        btnWebsite.setOnClickListener(this)
+        btnTermsCondition.setOnClickListener(this)
+        btncodeofconduct.setOnClickListener(this)
+        btnprivacypolicy.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val url = when(v?.id) {
+            R.id.btnGit -> getString(R.string.url_github)
+            R.id.btnSlack -> getString(R.string.url_slack)
+            R.id.btnWebsite -> getString(R.string.url_website)
+            R.id.btnTermsCondition -> getString(R.string.url_terms)
+            R.id.btncodeofconduct -> getString(R.string.url_code_of_conduct)
+            R.id.btnprivacypolicy -> getString(R.string.url_privacy)
+
+            else -> {
+                return
+            }
+        }
+
+        showProgress()
+        webView.loadUrl(url)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -45,4 +65,40 @@ class AboutActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        } else if(webView.visibility == View.VISIBLE) {
+            hideWebView()
+            return true
+        }
+
+        // exit activity
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun showWebView() {
+        scrollView.visibility = View.GONE
+        webView.visibility = View.VISIBLE
+        hideProgress()
+    }
+
+    private fun hideWebView() {
+        clearHistory = true
+        scrollView.visibility = View.VISIBLE
+        webView.visibility = View.GONE
+        hideProgress()
+    }
+
+    private fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+        webView.visibility = View.GONE
+        scrollView.visibility = View.GONE
+    }
+
+    private fun hideProgress() {
+        progressBar.visibility = View.GONE
+    }
 }
