535e45abe8e5527493e4a9abd1d9e3f15e8ecbc7
Ilya <veershtein@gmail.com>
2019-12-08 10:58:29 +0100

    feat: Add a new SplashScreen
    feat: Add a new SplashScreen

--- app/src/main/java/org/systers/mentorship/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/SplashScreenActivity.kt
@@ -2,9 +2,7 @@ package org.systers.mentorship.view.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import org.systers.mentorship.R
 import org.systers.mentorship.utils.PreferenceManager
 
 /**
@@ -12,32 +10,17 @@ import org.systers.mentorship.utils.PreferenceManager
  */
 class SplashScreenActivity : AppCompatActivity() {
 
-    private lateinit var handler: Handler
-    private lateinit var runnable: Runnable
-    private var SPLASH_DISPLAY_LENGTH: Long = 1000
-    private val preferenceManager: PreferenceManager = PreferenceManager()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
-        val intent = if (preferenceManager.authToken.isEmpty()) {
+        val intent = if (PreferenceManager().authToken.isEmpty()) {
             Intent(this, LoginActivity::class.java)
         } else {
             Intent(this, MainActivity::class.java)
         }
 
-        runnable = Runnable {
-            startActivity(intent)
-            finish()
-        }
-        handler = Handler()
-        handler.postDelayed(runnable, SPLASH_DISPLAY_LENGTH)
+        startActivity(intent)
+        finish()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        handler.removeCallbacks(runnable)
-    }
 }
