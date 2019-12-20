4e4915277d2b01ab7c5a522c7dc99b46177c7e67
Isabel Sieh <isabelsieh@gmail.com>
2019-12-20 15:29:52 +0800

    fix: remove unused SuppressLint imports

--- app/src/main/java/org/systers/mentorship/models/Relationship.kt
+++ app/src/main/java/org/systers/mentorship/models/Relationship.kt
@@ -1,6 +1,5 @@
 package org.systers.mentorship.models
 
-import android.annotation.SuppressLint
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
