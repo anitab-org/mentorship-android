004528448d02263e031834415728ff39905d61b6
Bartek Pacia <barpac02@gmail.com>
2020-01-05 01:03:52 +0100

    feat: improved SplashScreen logic

--- app/src/main/java/org/systers/mentorship/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/SplashScreenActivity.kt
@@ -11,15 +11,10 @@ import org.systers.mentorship.utils.PreferenceManager
  * This activity will show the organisation logo for sometime and then start the next activity
  */
 class SplashScreenActivity : AppCompatActivity() {
-
-    private lateinit var handler: Handler
-    private lateinit var runnable: Runnable
-    private var SPLASH_DISPLAY_LENGTH: Long = 1000
     private val preferenceManager: PreferenceManager = PreferenceManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
         val intent = if (preferenceManager.authToken.isEmpty()) {
             Intent(this, LoginActivity::class.java)
@@ -27,17 +22,7 @@ class SplashScreenActivity : AppCompatActivity() {
             Intent(this, MainActivity::class.java)
         }
 
-        runnable = Runnable {
-            startActivity(intent)
-            finish()
-        }
-        handler = Handler()
-        handler.postDelayed(runnable, SPLASH_DISPLAY_LENGTH)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        handler.removeCallbacks(runnable)
+        startActivity(intent)
+        finish()
     }
 }
