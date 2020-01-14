33cfbbca133946cb1b822cab950514b0bb4cb280
Bartek Pacia <barpac02@gmail.com>
2020-01-14 22:11:15 +0100

    feat: added sorting and filtering to MembersFragment

--- app/src/main/java/org/systers/mentorship/view/adapters/MembersAdapter.kt
+++ app/src/main/java/org/systers/mentorship/view/adapters/MembersAdapter.kt
@@ -1,10 +1,11 @@
 package org.systers.mentorship.view.adapters
 
-import androidx.annotation.NonNull
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_member_item.view.*
 import org.systers.mentorship.MentorshipApplication
 import org.systers.mentorship.R
@@ -13,15 +14,115 @@ import org.systers.mentorship.utils.NON_VALID_VALUE_REPLACEMENT
 
 /**
  * This class represents the adapter that fills in each view of the Members recyclerView
- * @param userList list of users to show
  * @param openDetailFunction function to be called when an item from Members list is clicked
  */
-class MembersAdapter (
-        private val userList: List<User>,
+class MembersAdapter constructor(
         private val openDetailFunction: (memberId: Int) -> Unit
 ) : RecyclerView.Adapter<MembersAdapter.MembersViewHolder>() {
 
     val context = MentorshipApplication.getContext()
+
+    var filter: Filter = Filter()
+        set(value) {
+            field = value
+
+            var newList: List<User> = initialUsers.toList()
+
+            // If filtering options are enabled, apply them
+            if (filter.needsMentoring || filter.availableToMentor) {
+                newList = newList
+                        .filter { it.needsMentoring == filter.needsMentoring }
+                        .filter { it.isAvailableToMentor == filter.availableToMentor }
+            }
+
+            if (filter.skills.isNotBlank()) {
+                newList = newList.filter {
+                    it.skills?.contains(filter.skills.trim(), ignoreCase = true) ?: false
+                }
+            }
+
+            currentUsers = newList.sorted(sort)
+        }
+
+    var sort: Sort = Sort.CreationTimeDescending
+        set(newSort) {
+            field = newSort
+
+            val newList = when (sort) {
+                Sort.NameAscending -> {
+                    currentUsers.toMutableList().sortedBy {
+                        it.name?.toLowerCase()
+                    }
+                }
+                Sort.NameDescending -> {
+                    currentUsers.toMutableList().sortedByDescending {
+                        it.name?.toLowerCase()
+                    }
+                }
+                Sort.CreationTimeAscending -> {
+                    val userListAscending = initialUsers.reversed()
+                    userListAscending
+                }
+                Sort.CreationTimeDescending -> initialUsers
+            }
+
+            currentUsers = newList
+        }
+
+    /**
+     * Sorts current list using passed [sort]
+     */
+    private fun List<User>.sorted(sort: Sort): List<User> {
+        return when (sort) {
+            Sort.NameAscending -> {
+                toMutableList().sortedBy {
+                    it.name?.toLowerCase()
+                }
+            }
+            Sort.NameDescending -> {
+                toMutableList().sortedByDescending {
+                    it.name?.toLowerCase()
+                }
+            }
+            Sort.CreationTimeAscending -> {
+                val userListAscending = this.reversed()
+                userListAscending
+            }
+            Sort.CreationTimeDescending -> this
+        }
+    }
+
+    /**
+     * Original user list, sorted by creation time in descending order
+     * **Rationale:** This methods exists only because the list returned by the API is sorted
+     * by creation time in descending order, but the User objects itself don't
+     * have any information about creation time.
+     */
+    var initialUsers: List<User> = arrayListOf()
+
+    /**
+     * Currently displayed user list, with sorting and filtering applied.
+     */
+    var currentUsers: List<User> = arrayListOf()
+        set(newUsers) {
+            val diffCallback = MembersDiffCallback(currentUsers, newUsers)
+            val diffResult = DiffUtil.calculateDiff(diffCallback)
+
+            field = newUsers
+            diffResult.dispatchUpdatesTo(this)
+        }
+
+    private fun getMentorshipAvailabilityText(availableToMentor: Boolean?, needMentoring: Boolean?): String {
+
+        if (availableToMentor != null && needMentoring != null) {
+            return if (availableToMentor && needMentoring) context.getString(R.string.available_to_mentor_and_mentee)
+            else if (availableToMentor) context.getString(R.string.only_available_to_mentor)
+            else if (needMentoring) context.getString(R.string.only_available_to_mentee)
+            else context.getString(R.string.not_available_to_mentor_or_mentee)
+        }
+
+        return context.getString(R.string.not_available_to_mentor_or_mentee)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MembersViewHolder =
             MembersViewHolder(
@@ -30,7 +131,7 @@ class MembersAdapter (
             )
 
     override fun onBindViewHolder(@NonNull holder: MembersViewHolder, position: Int) {
-        val item = userList[position]
+        val item = currentUsers[position]
         val itemView = holder.itemView
 
         itemView.tvName.text = item.name
@@ -45,23 +146,42 @@ class MembersAdapter (
         itemView.setOnClickListener { openDetailFunction(item.id!!) }
     }
 
-    override fun getItemCount(): Int = userList.size
+    override fun getItemCount(): Int = currentUsers.size
 
     /**
-     * This class holds a view for each item of the Members list
+     * Holds a view for each item of the Members list
      * @param itemView represents each view of Members list
      */
     class MembersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    private fun getMentorshipAvailabilityText(availableToMentor: Boolean?, needMentoring: Boolean?): String {
+    /**
+     * Represents available sorting methods.
+     */
+    enum class Sort(val number: Int) {
+        NameAscending(0), NameDescending(1), CreationTimeAscending(2), CreationTimeDescending(3);
 
-        if (availableToMentor != null && needMentoring != null) {
-            return if (availableToMentor && needMentoring) context.getString(R.string.available_to_mentor_and_mentee)
-            else if (availableToMentor) context.getString(R.string.only_available_to_mentor)
-            else if (needMentoring) context.getString(R.string.only_available_to_mentee)
-            else context.getString(R.string.not_available_to_mentor_or_mentee)
+        companion object {
+            private val map = values().associateBy(Sort::number)
+            /**
+             * Returns Sort enum corresponding to the passed integer [type]
+             */
+            fun fromInt(type: Int) = map[type]
         }
-
-        return context.getString(R.string.not_available_to_mentor_or_mentee)
     }
+
+    /**
+     * Represents available properties which the user list can be filtered by.
+     */
+    data class Filter(val needsMentoring: Boolean = false, val availableToMentor: Boolean = false, val skills: String = "")
+}
+
+class MembersDiffCallback(private val oldList: List<User>, private val newList: List<User>) : DiffUtil.Callback() {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldList[oldItemPosition] == newList[newItemPosition]
+
+    override fun getOldListSize() = oldList.size
+
+    override fun getNewListSize() = newList.size
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldList[oldItemPosition] == newList[newItemPosition]
+
 }
--- app/src/main/java/org/systers/mentorship/view/fragments/MembersFragment.kt
+++ app/src/main/java/org/systers/mentorship/view/fragments/MembersFragment.kt
@@ -1,12 +1,18 @@
 package org.systers.mentorship.view.fragments
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_members.*
 import org.systers.mentorship.R
 import org.systers.mentorship.utils.Constants
@@ -18,7 +24,7 @@ import org.systers.mentorship.viewmodels.MembersViewModel
 /**
  * The fragment is responsible for showing all the members of the system in a list format
  */
-class MembersFragment: BaseFragment() {
+class MembersFragment : BaseFragment() {
 
     companion object {
         /**
@@ -29,14 +35,31 @@ class MembersFragment: BaseFragment() {
 
     private lateinit var membersViewModel: MembersViewModel
 
+    private val openUserProfile: (Int) -> Unit =
+            { memberId ->
+                val intent = Intent(activity, MemberProfileActivity::class.java)
+                intent.putExtra(Constants.MEMBER_USER_ID, memberId)
+                startActivity(intent)
+            }
+
+    private val membersAdapter = MembersAdapter(openDetailFunction = openUserProfile)
+
+    private var successfulRequestCount = 0
+
     override fun getLayoutResourceId(): Int = R.layout.fragment_members
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        setHasOptionsMenu(true)
+
+        rvMembers.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = membersAdapter
+        }
+
         membersViewModel = ViewModelProviders.of(this).get(MembersViewModel::class.java)
-        membersViewModel.successful.observe(this, Observer {
-            successful ->
+        membersViewModel.successful.observe(this, Observer { successful ->
             (activity as MainActivity).hideProgressDialog()
             if (successful != null) {
                 if (successful) {
@@ -44,10 +67,10 @@ class MembersFragment: BaseFragment() {
                         tvEmptyList.text = getString(R.string.empty_members_list)
                         rvMembers.visibility = View.GONE
                     } else {
-                        rvMembers.apply {
-                            layoutManager = LinearLayoutManager(context)
-                            adapter = MembersAdapter(membersViewModel.userList, openUserProfile)
-                        }
+                        successfulRequestCount++
+                        if (successfulRequestCount == 1) membersAdapter.initialUsers = membersViewModel.userList
+
+                        membersAdapter.currentUsers = membersViewModel.userList
                         tvEmptyList.visibility = View.GONE
                     }
                 } else {
@@ -62,10 +85,69 @@ class MembersFragment: BaseFragment() {
         membersViewModel.getUsers()
     }
 
-    private val openUserProfile: (Int) -> Unit =
-            { memberId ->
-                val intent = Intent(activity, MemberProfileActivity::class.java)
-                intent.putExtra(Constants.MEMBER_USER_ID, memberId)
-                startActivity(intent)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_members, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        fun showFilterDialog() {
+            val view = layoutInflater.inflate(R.layout.dialog_filter_members, null, false)
+
+            val cbNeedsMentoring: CheckBox = view.findViewById(R.id.cbNeedsMentoring)
+            val cbAvailableToMentor: CheckBox = view.findViewById(R.id.cbAvailableToMentor)
+            val etSkills: EditText = view.findViewById(R.id.etSkills)
+
+            cbNeedsMentoring.isChecked = membersAdapter.filter.needsMentoring
+            cbAvailableToMentor.isChecked = membersAdapter.filter.availableToMentor
+            etSkills.setText(membersAdapter.filter.skills)
+
+            AlertDialog.Builder(baseActivity)
+                    .setTitle(R.string.filter_by)
+                    .setView(view)
+                    .setPositiveButton(R.string.filter_by) { dialog, _ ->
+                        val needsMentoring = cbNeedsMentoring.isChecked
+                        val availableToMentor = cbAvailableToMentor.isChecked
+                        val skills = etSkills.text.toString()
+
+
+                        val filter = MembersAdapter.Filter(needsMentoring, availableToMentor, skills)
+                        membersAdapter.filter = filter
+
+                        dialog.dismiss()
+                    }
+                    .show()
+        }
+
+        fun showSortDialog() {
+            val items = arrayOf(
+                    getString(R.string.name_descending),
+                    getString(R.string.name_ascending),
+                    getString(R.string.creation_date_ascending),
+                    getString(R.string.creation_date_descending)
+            )
+
+            AlertDialog.Builder(baseActivity)
+                    .setTitle(R.string.sort_by)
+                    .setSingleChoiceItems(items, membersAdapter.sort.number) { dialog, i ->
+                        val sort = MembersAdapter.Sort.fromInt(i)
+                                ?: MembersAdapter.Sort.NameAscending
+                        membersAdapter.sort = sort
+                        dialog.dismiss()
+                    }
+                    .show()
+        }
+
+        return when (item.itemId) {
+            R.id.menu_filter -> {
+                showFilterDialog()
+                true
             }
+            R.id.menu_sort -> {
+                showSortDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
