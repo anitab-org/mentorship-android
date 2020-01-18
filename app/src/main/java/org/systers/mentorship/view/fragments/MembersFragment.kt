b8de0ac347eca4b0b9c27dedf2a3f2a680332d52
daksha-aeer <dakshaaeer@gmail.com>
2020-01-18 14:52:05 +0530

    feat: Add sort and filter feature for users
    Sort: Alphabetical order and date based
    Filter: Mentor, mentee, both mentor and mentee
    
    Working
    1. FloatActionButtons for sort and filter
    2. Listeners attached to buttons. They filter list when clicked.

--- app/src/main/java/org/systers/mentorship/view/fragments/MembersFragment.kt
+++ app/src/main/java/org/systers/mentorship/view/fragments/MembersFragment.kt
@@ -1,5 +1,6 @@
 package org.systers.mentorship.view.fragments
 
+import android.content.DialogInterface
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import android.content.Intent
@@ -7,8 +8,12 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.fragment_members.*
+import org.systers.mentorship.MentorshipApplication
 import org.systers.mentorship.R
+import org.systers.mentorship.models.User
 import org.systers.mentorship.utils.Constants
 import org.systers.mentorship.view.activities.MainActivity
 import org.systers.mentorship.view.activities.MemberProfileActivity
@@ -28,6 +33,9 @@ class MembersFragment: BaseFragment() {
     }
 
     private lateinit var membersViewModel: MembersViewModel
+    private lateinit var changedList: List<User>
+    private var filterCheckedItem = -1
+    private var sortCheckedItem = -1
 
     override fun getLayoutResourceId(): Int = R.layout.fragment_members
 
@@ -49,6 +57,9 @@ class MembersFragment: BaseFragment() {
                             adapter = MembersAdapter(membersViewModel.userList, openUserProfile)
                         }
                         tvEmptyList.visibility = View.GONE
+                        changedList = membersViewModel.userList
+                        filterUsers()
+                        sortUsers()
                     }
                 } else {
                     view?.let {
@@ -60,6 +71,7 @@ class MembersFragment: BaseFragment() {
 
         (activity as MainActivity).showProgressDialog(getString(R.string.fetching_users))
         membersViewModel.getUsers()
+
     }
 
     private val openUserProfile: (Int) -> Unit =
@@ -68,4 +80,99 @@ class MembersFragment: BaseFragment() {
                 intent.putExtra(Constants.MEMBER_USER_ID, memberId)
                 startActivity(intent)
             }
+
+    private fun sortUsers(){
+        fabSortUsers.setOnClickListener {
+            val builder = AlertDialog.Builder(it.context)
+            builder.setTitle(getString(R.string.filter_title))
+            var sortOptions = arrayOf<CharSequence>(getString(R.string.sort_alphabetical), getString(R.string.sort_date))
+            builder.setSingleChoiceItems(sortOptions, sortCheckedItem, DialogInterface.OnClickListener { dialog, item ->
+                when (item) {
+                    0 ->{ //sort alphabetically
+                        Toast.makeText(MentorshipApplication.getContext(), getString(R.string.sort_alphabetical), Toast.LENGTH_LONG).show()
+                        val newList = changedList.sortedBy {
+                            it.name
+                        }
+                        changedList = newList
+                        rvMembers.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = MembersAdapter(newList, openUserProfile)
+                        }
+                    }
+                    1 ->{ //sort by join date(default)
+                        Toast.makeText(MentorshipApplication.getContext(), getString(R.string.sort_date), Toast.LENGTH_LONG).show()
+                        rvMembers.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = MembersAdapter(membersViewModel.userList, openUserProfile)
+                        }
+                        changedList = membersViewModel.userList
+                    }
+                }
+                sortCheckedItem = item
+                dialog.dismiss()
+            })
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+    }
+
+    private fun filterUsers(){
+        fabFilterUsers.setOnClickListener {
+            val builder = AlertDialog.Builder(it.context)
+            builder.setTitle(getString(R.string.filter_title))
+            val filterOptions = arrayOf<CharSequence>(
+                    getString(R.string.mentor), getString(R.string.mentee),
+                    getString(R.string.mentor_and_mentee), getString(R.string.clear)
+            )
+            builder.setSingleChoiceItems(filterOptions, filterCheckedItem, DialogInterface.OnClickListener { dialog, item ->
+                when (item) {
+                    0 ->{ //for mentor
+                        Toast.makeText(MentorshipApplication.getContext(), getString(R.string.mentor), Toast.LENGTH_LONG).show()
+                        val newList = changedList.filter {
+                            it.isAvailableToMentor ?: false //assign false by default if value is null
+                        }
+                        changedList = newList
+                        rvMembers.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = MembersAdapter(newList, openUserProfile)
+                        }
+                    }
+                    1 ->{ //for mentee
+                        Toast.makeText(MentorshipApplication.getContext(), getString(R.string.mentee), Toast.LENGTH_LONG).show()
+                        val newList = changedList.filter {
+                            it.needsMentoring ?: false //assign false by default if value is null
+                        }
+                        changedList = newList
+                        rvMembers.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = MembersAdapter(newList, openUserProfile)
+                        }
+                    }
+                    2 ->{ //for mentor and mentee
+                        Toast.makeText(MentorshipApplication.getContext(), getString(R.string.mentor_and_mentee), Toast.LENGTH_LONG).show()
+                        val newList = changedList.filter {
+                            it.needsMentoring ?: false && it.isAvailableToMentor ?: false
+                        }
+                        changedList = newList
+                        rvMembers.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = MembersAdapter(newList, openUserProfile)
+                        }
+                    }
+                    3 ->{ //clear filter
+                        Toast.makeText(MentorshipApplication.getContext(), getString(R.string.clear), Toast.LENGTH_LONG).show()
+                        rvMembers.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = MembersAdapter(membersViewModel.userList, openUserProfile)
+                        }
+                        changedList = membersViewModel.userList
+                    }
+                }
+                filterCheckedItem = item
+                dialog.dismiss()
+            })
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+    }
 }
