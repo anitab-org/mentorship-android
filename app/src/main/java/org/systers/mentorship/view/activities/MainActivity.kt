690664b8f312809c4348c53692d63e3b16659d11
Ilya Maier <ilya.maier@outlook.com>
2020-01-19 17:04:46 +0100

    feat: Add ViewPager in MainActivity
    feat: Add ViewPager in MainActivity

--- app/src/main/java/org/systers/mentorship/view/activities/MainActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/MainActivity.kt
@@ -1,28 +1,54 @@
 package org.systers.mentorship.view.activities
 
+import android.annotation.TargetApi
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.systers.mentorship.R
-import org.systers.mentorship.utils.PreferenceManager
+import org.systers.mentorship.utils.Constants.NUM_PAGES
+import org.systers.mentorship.utils.DepthPageTransformer
 import org.systers.mentorship.view.fragments.*
 
 /**
  * This activity has the bottom navigation which allows the user to switch between fragments
  */
-class MainActivity: BaseActivity() {
+class MainActivity : BaseActivity() {
 
     private var atHome = true
 
-    private val preferenceManager: PreferenceManager = PreferenceManager()
-
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPagerMain.adapter = ScreenSlidePagerAdapter(this)
+        viewPagerMain.setPageTransformer(DepthPageTransformer())
+        // for BottomNavigationBar to represent the new fragment
+        viewPagerMain.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                bottomNavigation.selectedItemId =
+                        when (position) {
+                            0 -> R.id.navigation_home
+                            1 -> R.id.navigation_profile
+                            2 -> R.id.navigation_relation
+                            3 -> R.id.navigation_members
+                            4 -> R.id.navigation_requests
+                            else -> R.id.navigation_home
+                        }
+                atHome = position == 0
+            }
+        })
+        // in order to allow to use ViewPager inside of a ViewPager
+        viewPagerMain.reduceDragSensitivity()
 
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
@@ -35,40 +61,35 @@ class MainActivity: BaseActivity() {
 
     private val mOnNavigationItemSelectedListener =
             BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                replaceFragment(R.id.contentFrame, HomeFragment.newInstance(),
-                        R.string.fragment_title_home)
-                atHome = true
-                return@OnNavigationItemSelectedListener true
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        viewPagerMain.currentItem = 0
+                        atHome = true
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_profile -> {
+                        viewPagerMain.currentItem = 1
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_relation -> {
+                        viewPagerMain.currentItem = 2
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_members -> {
+                        viewPagerMain.currentItem = 3
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.navigation_requests -> {
+                        viewPagerMain.currentItem = 4
+                        atHome = false
+                        return@OnNavigationItemSelectedListener true
+                    }
+                }
+                false
             }
-            R.id.navigation_profile -> {
-                replaceFragment(R.id.contentFrame, ProfileFragment.newInstance(),
-                        R.string.fragment_title_profile)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_relation -> {
-                replaceFragment(R.id.contentFrame, RelationPagerFragment.newInstance(),
-                        R.string.fragment_title_relation)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_members -> {
-                replaceFragment(R.id.contentFrame, MembersFragment.newInstance(),
-                        R.string.fragment_title_members)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_requests -> {
-                replaceFragment(R.id.contentFrame, RequestsFragment.newInstance(),
-                        R.string.fragment_title_requests)
-                atHome = false
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
@@ -87,7 +108,7 @@ class MainActivity: BaseActivity() {
     private fun showHomeFragment() {
         atHome = true
         bottomNavigation.selectedItemId = R.id.navigation_home
-        replaceFragment(R.id.contentFrame, HomeFragment.newInstance(), R.string.fragment_title_home)
+        viewPagerMain.currentItem = 0
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -103,4 +124,32 @@ class MainActivity: BaseActivity() {
             super.onBackPressed()
         }
     }
+
+    private fun ViewPager2.reduceDragSensitivity() {
+        val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
+        recyclerViewField.isAccessible = true
+        val recyclerView = recyclerViewField.get(this) as RecyclerView
+
+        val touchSlopField = RecyclerView::class.java.getDeclaredField("mTouchSlop")
+        touchSlopField.isAccessible = true
+        val touchSlop = touchSlopField.get(recyclerView) as Int
+        touchSlopField.set(recyclerView, (touchSlop*1.6).toInt())       // "1.6" was obtained experimentally
+    }
+
+    /**
+     * A simple pager adapter that represents 5 main fragments
+     */
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+        override fun createFragment(position: Int) =
+                when (position) {
+                    0 -> HomeFragment.newInstance()
+                    1 -> ProfileFragment.newInstance()
+                    2 -> RelationPagerFragment.newInstance()
+                    3 -> MembersFragment.newInstance()
+                    4 -> RequestsFragment.newInstance()
+                    else -> HomeFragment.newInstance()
+                }
+    }
+
 }
