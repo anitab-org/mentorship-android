130b9bb7971d887843d01f5b8a7200a493ed05fb
atharwa-24 <atharwakharkar@gmail.com>
2020-08-25 14:39:23 +0530

    Removed unused Importers and some modifications
    this commit removes the unused "Delete Account" options function and also
    the settingsviewmodel file in order to remove the Delete Account feature
    from root
    
    Signed-off-by: atharwa-24 <atharwakharkar@gmail.com>

--- app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
@@ -3,23 +3,23 @@ package org.systers.mentorship.view.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
-import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.systers.mentorship.R
 import org.systers.mentorship.utils.PreferenceManager
 import org.systers.mentorship.view.fragments.ChangePasswordFragment
-import org.systers.mentorship.viewmodels.SettingsViewModel
+
 
 class SettingsActivity : BaseActivity() {
 
     private val preferenceManager: PreferenceManager = PreferenceManager()
-    private lateinit var settingsViewModel: SettingsViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+
+
         supportActionBar?.title = getString(R.string.settings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
