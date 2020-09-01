044f73eaea456b879fe95969995b2fc3ddb5c667
justdvnsh <justdvnsh2208@gmail.com>
2020-09-02 01:21:59 +0530

    test: Added Test for Change Password Fragment.
    body: This test checks that errors are being correclty displayed when passwords do not match. Fix for Issue #804 - Write Espresso Test for Change Password (b)

--- app/src/androidTest/java/org/systers/mentorship/view/fragments/ChangePasswordFragmentTest.kt
+++ app/src/androidTest/java/org/systers/mentorship/view/fragments/ChangePasswordFragmentTest.kt
@@ -26,6 +26,7 @@ import org.systers.mentorship.view.activities.SettingsActivity
 class ChangePasswordFragmentTest {
 
     private val PASSWORD_TOO_WEAK = "Your password is too weak! Use at least one small and capital letter, one number and one special sign!"
+    private val PASSWORDS_DO_NOT_MATCH = "Passwords do not match"
 
     // Start the SettingsActivity
     @get:Rule
@@ -50,7 +51,7 @@ class ChangePasswordFragmentTest {
         // launch the fragment
         ChangePasswordFragment.newInstance().show(activityTestRule.activity.supportFragmentManager, null)
 
-        enter_credentials("ab")
+        enter_credentials(R.id.tilNewPassword, "ab")
 
         // clicks the OK button in the Dialog Fragment
         onView(withText(R.string.ok)).inRoot(isDialog())
@@ -60,12 +61,31 @@ class ChangePasswordFragmentTest {
         onView(withId(R.id.tilNewPassword)).check(matches(hasTextInputLayoutErrorText(PASSWORD_TOO_WEAK)))
     }
 
+    /*
+    * This test checks that errors are being displayed correctly when the password do not match
+    * */
+    @Test
+    fun check_if_errors_are_shown_when_passwords_do_not_match() {
+        // launch the Fragment
+        ChangePasswordFragment.newInstance().show(activityTestRule.activity.supportFragmentManager, null)
+
+        enter_credentials(R.id.tilNewPassword,"AnitaB.org@2020")
+        enter_credentials(R.id.tilConfirmPassword, "AnitaB.org")
+
+        // clicks the OK button in the Dialog Fragment
+        onView(withText(R.string.ok)).inRoot(isDialog())
+                .check(matches(isDisplayed())).perform(click())
+
+        // Checks if the errors are showing with the correct text
+        onView(withId(R.id.tilConfirmPassword)).check(matches(hasTextInputLayoutErrorText(PASSWORDS_DO_NOT_MATCH)))
+    }
+
     // --- Helper Methods --- //
 
     /*
     * helper method to enter password
     * */
-    private fun enter_credentials(password: String) = findEditTextInTextInputLayout(R.id.tilNewPassword).run {
+    private fun enter_credentials(id: Int, password: String) = findEditTextInTextInputLayout(id).run {
         check(matches(isDisplayed()))
         perform(click())
         perform(typeText(password))
