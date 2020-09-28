05c93a3c56b9833dec1fbf0fd5c9ec530ce49657
Saarthak Seth <saarthakseth@gmail.com>
2020-09-29 00:03:42 +0530

    fix : corrected the message and the generation of unnecessary intent on entering wrong password

--- app/src/main/java/org/systers/mentorship/remote/TokenAuthenticator.kt
+++ app/src/main/java/org/systers/mentorship/remote/TokenAuthenticator.kt
@@ -18,6 +18,14 @@ class TokenAuthenticator: Authenticator{
     override fun authenticate(route: Route, response: Response): Request? {
         if (response.code() == 401) {
             preferenceManager.clear()
+
+            if(response.request().url().encodedPath() == "/login" || response.request().url().encodedPath() == "/register")
+                return null
+
+            val intent = Intent(MentorshipApplication.getContext(), LoginActivity::class.java)
+            intent.putExtra(Constants.TOKEN_EXPIRED_EXTRA, 0)
+            ContextCompat.startActivity(MentorshipApplication.getContext(), intent, null)
+
             return null
         }
         return response.request().newBuilder().build()
