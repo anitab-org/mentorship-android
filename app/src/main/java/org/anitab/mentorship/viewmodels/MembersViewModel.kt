a28a2365339ae43d7f83d2709463664ec51031f5
bosankus <ankush@androidplay.in>
2022-10-15 09:49:18 +0530

    Added mediatior livedata to manage user filtered data

--- app/src/main/java/org/anitab/mentorship/viewmodels/MembersViewModel.kt
+++ app/src/main/java/org/anitab/mentorship/viewmodels/MembersViewModel.kt
@@ -1,10 +1,12 @@
 package org.anitab.mentorship.viewmodels
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.filter
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.map
 import org.anitab.mentorship.models.User
 import org.anitab.mentorship.remote.datamanager.UserDataManager
 
@@ -13,7 +15,57 @@ import org.anitab.mentorship.remote.datamanager.UserDataManager
  */
 class MembersViewModel(userDataManager: UserDataManager) : ViewModel() {
 
-    private var _userListLiveData = userDataManager.getUsers().cachedIn(viewModelScope)
-    val userListLiveData: Flow<PagingData<User>> = _userListLiveData
+    private val userListNoFilter: LiveData<PagingData<User>> =
+        userDataManager.getAllUsers()
 
+    private val userLisFilterByNeedMentoring: LiveData<PagingData<User>> =
+        userDataManager.getUsersWhoAreNeedMentoring()
+
+    private val userListFilterByAvailableToMentor: LiveData<PagingData<User>> =
+        userDataManager.getUsersWhoAreAvailableToMentor()
+
+    private val userListFilterByHaveSkill: LiveData<PagingData<User>> =
+        userDataManager.getUserWhoHaveSkills()
+
+    val userList = MediatorLiveData<PagingData<User>>()
+
+    private var selectedUserFilter = ListFilter.NEED_MENTORING
+
+
+    init {
+        userList.addSource(userListNoFilter) { list ->
+            if (selectedUserFilter == ListFilter.NO_FILTER) {
+                list?.let { userList.value = it }
+            }
+        }
+
+        userList.addSource(userLisFilterByNeedMentoring) { list ->
+            if (selectedUserFilter == ListFilter.NEED_MENTORING) {
+                list?.let { userList.value = it }
+            }
+        }
+
+        userList.addSource(userListFilterByAvailableToMentor) { list ->
+            if (selectedUserFilter == ListFilter.AVAILABLE_TO_MENTOR) {
+                list?.let { userList.value = it }
+            }
+        }
+
+        userList.addSource(userListFilterByHaveSkill) { list ->
+            if (selectedUserFilter == ListFilter.HAVE_SKILL) {
+                list?.let { userList.value = it }
+            }
+        }
+    }
+
+    fun getFilteredUserList(filter: ListFilter) = when(filter) {
+        ListFilter.NO_FILTER -> userListNoFilter.value?.let { userList.value = it }
+        ListFilter.NEED_MENTORING -> userLisFilterByNeedMentoring.value?.let { userList.value = it }
+        ListFilter.AVAILABLE_TO_MENTOR -> userListFilterByAvailableToMentor.value?.let { userList.value = it }
+        ListFilter.HAVE_SKILL -> userListFilterByHaveSkill.value?.let { userList.value = it }
+    }.also { selectedUserFilter = filter }
+}
+
+enum class ListFilter {
+    NO_FILTER, NEED_MENTORING, AVAILABLE_TO_MENTOR, HAVE_SKILL
 }
